64a7d3a2e334941134294e37ef25a864ef0abab8
Veren Damyanov <vd@itlabs.bg>
2017-02-04 15:30:53 +0200

    Removed NetDevice get_interface() and refactored get_device().

--- tests/integration/sim_tests.py
+++ tests/integration/sim_tests.py
@@ -38,19 +38,19 @@ class TestPyLtcFake(unittest.TestCase):
         self.assertEqual(expected, fake_test.result)
 
     def test_upload_single_port_tcp(self):
-        fake_test = LtcSimulateTargetRun(['tc', '-i', 'lo1', '-c', '--upload', 'tcp:dport:9100:1mbit'])
+        fake_test = LtcSimulateTargetRun(['tc', '-i', 'lo', '-c', '--upload', 'tcp:dport:9100:1mbit'])
         fake_test.run()
         expected = [
-            'tc qdisc del dev lo1 root',
-            'tc qdisc add dev lo1 root handle 1:0 htb',
-            'tc class add dev lo1 parent 1:0 classid 1:1 htb rate 15gbit',
-            'tc class add dev lo1 parent 1:0 classid 1:2 htb rate 15gbit',
-            'tc filter add dev lo1 parent 1:0 protocol ip prio 1 u32 match ip protocol 6 0xff flowid 1:1',
-            'tc filter add dev lo1 parent 1:0 protocol ip prio 2 u32 match ip protocol 17 0xff flowid 1:2',
-            'tc qdisc add dev lo1 parent 1:1 handle 2:0 htb',
-            'tc qdisc add dev lo1 parent 1:2 handle 3:0 htb',
-            'tc class add dev lo1 parent 2:0 classid 2:1 htb rate 1mbit',
-            'tc filter add dev lo1 parent 2:0 protocol ip prio 1 u32 match ip dport 9100 0xffff flowid 2:1'
+            'tc qdisc del dev lo root',
+            'tc qdisc add dev lo root handle 1:0 htb',
+            'tc class add dev lo parent 1:0 classid 1:1 htb rate 15gbit',
+            'tc class add dev lo parent 1:0 classid 1:2 htb rate 15gbit',
+            'tc filter add dev lo parent 1:0 protocol ip prio 1 u32 match ip protocol 6 0xff flowid 1:1',
+            'tc filter add dev lo parent 1:0 protocol ip prio 2 u32 match ip protocol 17 0xff flowid 1:2',
+            'tc qdisc add dev lo parent 1:1 handle 2:0 htb',
+            'tc qdisc add dev lo parent 1:2 handle 3:0 htb',
+            'tc class add dev lo parent 2:0 classid 2:1 htb rate 1mbit',
+            'tc filter add dev lo parent 2:0 protocol ip prio 1 u32 match ip dport 9100 0xffff flowid 2:1'
         ]
         self.assertEqual(expected, fake_test.result)
 
