6ca9887ad517abd30b8f95c03a52029e12fb94f1
Chad Phillips <chad@apartmentlines.com>
2017-02-25 20:44:50 -0800

    update reference in example of using a profile to one in the example profile

--- examples/profile.py
+++ examples/profile.py
@@ -14,5 +14,5 @@ from pyltc.core.tfactory import printing_target_factory
 TrafficControl.init()
 simnet = TrafficControl.get_plugin('simnet', target_factory=printing_target_factory)
 simnet.configure(clear=True, verbose=True, ifbdevice='ifb2')  # as usual, first set general options
-simnet.load_profile('4g-sym-egress', config_file=MY_CONFIG_FILE)  # configure using the profile.
+simnet.load_profile('4g-upload', config_file=MY_CONFIG_FILE)  # configure using the profile.
 simnet.marshal()
