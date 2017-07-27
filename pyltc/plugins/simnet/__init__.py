ya55en/py-linux-traffic-control

commit 6459599cf0fb39478125472a253865404b50e679

#22: Refactor simnet into a plugin package

pyltc/plugins/simnet/__init__.py
@@ -1,0 +1,5 @@
+"""
+
+"""
+
+from .core import plugin_main, SimNetPlugin

pyltc/plugins/__init__.py
@@ -1,0 +1,29 @@
+"""
+PyLTC plugins directory.
+
+"""
+from os.path import abspath, normpath, dirname, basename, isfile, isdir, join as pjoin
+
+
+import os
+
+__all__ = []
+
+for mod_ in os.listdir(dirname(__file__)):
+    if isdir(pjoin(dirname(__file__), mod_)) and \
+            isfile(pjoin(dirname(__file__), mod_, '__init__.py')):
+        mod_ = "pyltc.plugins.{}".format(mod_)
+        __import__(mod_, locals(), globals())
+        __all__.append(mod_)
+
+    elif mod_[-3:] == '.py' and mod_ != '__init__.py':
+        __import__(mod_[:-3], locals(), globals())
+        __all__.append(mod_[:-3])
+
+    else:
+        print("Skipped plugin entry:", mod_)
+
+
+del mod_
+
+print(__all__)

pyltc/plugins/simnet/core.py
@@ -15,10 +15,9 @@ import argparse
 
 from pyltc.conf import CONFIG_PATHS, __build__, __version__
 from parser import ParserError
-from pyltc.util.cmdline import CommandLine
 from pyltc.util.confparser import ConfigParser
-from pyltc.core.netdevice import DeviceManager, NetDevice, NetDeviceNotFound
-from pyltc.plugins.simnet_util import BranchParser
+from pyltc.core.netdevice import DeviceManager, NetDevice
+from pyltc.plugins.simnet.util import BranchParser
 
 #: netem (the qdisc that simulates special network conditions) works for a
 # default of 1000 packets. This was a source of problems and the workaround

pyltc/plugins/simnet/util.py
@@ -26,10 +26,13 @@ class BranchParser(object):
         match = regex.match(branch_str)
         if not match:
             raise ParsingError("Invalid upload/download argument: {!r}".format(branch_str))
+
         if not match.group(2) and match.group(3) != 'all':
             raise ParsingError("Port type not found in {!r} (may be omitted only if range is 'all')".format(branch_str))
+
         if not (match.group(4) or match.group(5)):
             raise ParsingError('Either RATE, JITTER or both must be present in {!r}'.format(branch_str))
+
         self._branch = dict()
         self._branch['protocol'] = match.group(1)
         orig_porttype = match.group(2).lstrip(':') if match.group(2) else match.group(2)

commit a7c2161a3636e5169467206a3b38b4d21b69f088

#22 Fix tests modules' imports broken by 6459599cf0

pyltc/plugins/simnet/__init__.py
@@ -2,4 +2,4 @@
 
 """
 
-from .core import plugin_main, SimNetPlugin
+from .core import plugin_main, SimNetPlugin, parse_args

tests/integration/live_tests.py
@@ -18,7 +18,7 @@ if not REPO_ROOT in sys.path:
 
 from pyltc.core import DIR_EGRESS
 from pyltc.main import pyltc_entry_point
-from pyltc.plugins.simnet_util import BranchParser
+from pyltc.plugins.simnet.util import BranchParser
 from pyltc.util.rates import convert2bps
 from pyltc.util.counter import Counter
 from tests.util.base import LtcLiveTargetRun

tests/plugins_tests/test_util.py
@@ -5,7 +5,7 @@ Unit tests for the pytc helper stuff.
 import unittest
 from configparser import ParsingError
 
-from pyltc.plugins.simnet_util import BranchParser
+from pyltc.plugins.simnet.util import BranchParser
 
 
 class TestBranchParser(unittest.TestCase):

tests/util/base.py
@@ -6,7 +6,7 @@ from pyltc.plugins.simnet import parse_args
 from pyltc.core.target import TcTarget, TcCommandTarget, TcFileTarget
 from pyltc.main import pyltc_entry_point
 from tests.util.iperf_proc import TCPNetPerfTest, UDPNetPerfTest
-from pyltc.plugins.simnet_util import BranchParser
+from pyltc.plugins.simnet.util import BranchParser
 
 
 class TcTestTarget(TcTarget):