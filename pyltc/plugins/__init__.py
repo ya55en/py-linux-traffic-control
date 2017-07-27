cb094aa4e1562fb66f1f115739571f94d8ee9dc4
Yassen Damyanov <yd@itlabs.bg>
2017-07-27 17:45:32 +0300

    #22: Cleanup plugins initialization routine

--- pyltc/plugins/__init__.py
+++ pyltc/plugins/__init__.py
@@ -1,29 +1,31 @@
 """
-PyLTC plugins directory.
+PyLTC plugins initialization.
+
+We scan the ``pyltc/plugins`` directory and import any discovered packages.
 
 """
-from os.path import abspath, normpath, dirname, basename, isfile, isdir, join as pjoin
-
-
 import os
+from os.path import dirname, isfile, isdir, join as pjoin
+
 
 __all__ = []
 
-for mod_ in os.listdir(dirname(__file__)):
-    if isdir(pjoin(dirname(__file__), mod_)) and \
-            isfile(pjoin(dirname(__file__), mod_, '__init__.py')):
-        mod_ = "pyltc.plugins.{}".format(mod_)
-        __import__(mod_, locals(), globals())
-        __all__.append(mod_)
 
-    elif mod_[-3:] == '.py' and mod_ != '__init__.py':
-        __import__(mod_[:-3], locals(), globals())
-        __all__.append(mod_[:-3])
+for _mod in os.listdir(dirname(__file__)):
+    if isdir(pjoin(dirname(__file__), _mod)) and \
+            isfile(pjoin(dirname(__file__), _mod, '__init__.py')):
+        _mod = "pyltc.plugins.{}".format(_mod)
+        __import__(_mod, locals(), globals())
+        __all__.append(_mod)
+        print("Added", _mod)
+
+    elif _mod[-3:] == '.py' and _mod != '__init__.py':
+        __import__(_mod[:-3], locals(), globals())
+        __all__.append(_mod[:-3])
+        print("Added", _mod[:-3])
 
     else:
-        print("Skipped plugin entry:", mod_)
+        print("Skipped plugin entry:", _mod)
 
 
-del mod_
-
-print(__all__)
+del _mod
